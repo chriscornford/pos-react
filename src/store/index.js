405ec8dc6715e059d64e52d4658b6b158e20af6f
Chris Cornford <chris.j.cornford@gmail.com>
2018-07-13 08:38:04 +0100

    Add store

---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import { offline } from '@redux-offline/redux-offline';
+import defaultConfig from '@redux-offline/redux-offline/lib/defaults';
+
+import rootReducer from '../reducers'
+
+const config = {
+    ...defaultConfig,
+    retry(_action, retries) {
+        return (retries + 1) * 1000;
+    },
+    returnPromises: true
+};
+
+function tickMiddleware(store) {
+    return next => action => {
+        if (action.type === 'Offline/SCHEDULE_RETRY') {
+            const intervalId = setInterval(() => {
+                store.dispatch({ type: 'TICK' });
+            }, 1000);
+            setTimeout(() => clearInterval(intervalId), action.payload.delay);
+        }
+        return next(action);
+    };
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(
+    rootReducer,
+    composeEnhancers(
+        offline(config),
+        applyMiddleware(
+            tickMiddleware
+        ),
+    )
+);
